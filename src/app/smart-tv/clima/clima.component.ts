6ccc373a429a2ddd9ce4cd26d69984041f70e275
FernandoBitValencia <fernando@bitvalencia.com>
2020-06-19 21:04:12 +0200

    Edit component clima

--- src/app/smart-tv/clima/clima.component.ts
+++ src/app/smart-tv/clima/clima.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { ClimaService } from "src/app/services/clima.service";
+import { platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
 
 @Component({
   selector: "app-clima",
@@ -8,8 +9,16 @@ import { ClimaService } from "src/app/services/clima.service";
 })
 export class ClimaComponent implements OnInit {
   @Input() dia: any;
+  date: any;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.parseDate();
+    console.log(this.dia);
+  }
+
+  parseDate() {
+    this.date = this.dia["@attributes"].fecha.split("-").reverse().join("/");
+  }
 }
