70dc96c1ebc755b4ba22d6eb92dee822f6f6e88c
Josecongas <webcongas@gmail.com>
2020-06-19 21:54:04 +0200

    pipes de fechas

---
+++ src/app/pipes/fecha-clima.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "fechaClima",
+})
+export class FechaClimaPipe implements PipeTransform {
+  transform(value: string): any {
+    const dias = [
+      "Domingo",
+      "Lunes",
+      "Martes",
+      "Miércoles",
+      "Jueves",
+      "Viernes",
+      "Sábado",
+    ];
+    const re = /\//gi;
+    const dateParts: any = value.split("/");
+    const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+
+    return dias[dateObject.getDay()];
+  }
+}
